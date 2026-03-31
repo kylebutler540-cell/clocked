9d78725f876166d08f64696f434e7b74a87f7630
kylebutler540-cell <kylebutler540@gmail.com>
2026-03-31 16:50:41 -0400

    chore: bump frontend to v43

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,9 +17,9 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name]-[hash]-v42.js`,
-        chunkFileNames: `assets/[name]-[hash]-v42.js`,
-        assetFileNames: `assets/[name]-[hash]-v17.[ext]`,
+        entryFileNames: `assets/[name]-[hash]-v43.js`,
+        chunkFileNames: `assets/[name]-[hash]-v43.js`,
+        assetFileNames: `assets/[name]-[hash]-v18.[ext]`,
       },
     },
   },
